a1fce1244c45a3c3c024107df5d3ea188c1d43a6
Mathias Fontain <mathias.fontain@vol.at>
2024-07-10 11:40:31 +0200

    first push

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import UndergroundMap from '@/components/UndergroundMap.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <UndergroundMap />
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ src/components/UndergroundMap.vue
@@ -0,0 +1,81 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 3508 2481"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xml:space="preserve"
+    xmlns:serif="http://www.serif.com/"
+    style="
+      fill-rule: evenodd;
+      clip-rule: evenodd;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      stroke-miterlimit: 1.5;
+    "
+  >
+    <g id="waters">
+      <path
+        v-for="water in waters"
+        :key="water.id"
+        :d="water.d"
+        :stroke-width="water.width"
+        stroke="#dbf2f3"
+        fill="none"
+      />
+    </g>
+
+    <g id="lines">
+      <path
+        v-for="line in lines"
+        :key="line.id"
+        :d="line.d"
+        fill="none"
+        :stroke="line.color"
+        stroke-width="20"
+      />
+    </g>
+
+    <g id="stations">
+      <circle
+        v-for="station in stations"
+        :key="station.name"
+        :cx="station.cx"
+        :cy="station.cy"
+        r="15"
+        stroke="#000"
+        stroke-width="5"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+import waters from '../data/waters.json'
+import lines from '../data/lines.json'
+import stations from '../data/stations.json'
+
+export default {
+  data() {
+    return {
+      waters,
+      lines,
+      stations
+    }
+  }
+}
+</script>
+
+<style scoped>
+circle {
+  cursor: pointer;
+  transition: all 0.3s ease;
+  fill: white;
+}
+
+circle:hover {
+  fill: grey;
+}
+</style>
