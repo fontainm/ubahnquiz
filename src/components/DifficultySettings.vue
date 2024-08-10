34da3b2fb5006eaa49ab109dea6fb319c077971b
Mathias Fontain <mathias.fontain@vol.at>
2024-08-10 08:04:00 +0200

    update highlight

--- src/components/DifficultySettings.vue
+++ src/components/DifficultySettings.vue
@@ -54,7 +54,7 @@ import { difficulties } from '../consts/difficulties'
     </div>
     <button
       :class="{
-        highlight: selectedDifficulty.id !== mainStore.selectedDifficulty.id || !showApply,
+        highlight: selectedDifficulty.id !== mainStore.selectedDifficulty.id && showApply,
         success: selectedDifficulty.id !== mainStore.selectedDifficulty.id && showApply
       }"
       @click="handleClickReset"
